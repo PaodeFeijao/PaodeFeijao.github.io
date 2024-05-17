c0cc4389b748ca547c65527e83677a0f22f33324
Gabriel Augusto de Andrade <71901565+PaodeFeijao@users.noreply.github.com>
2024-05-17 18:24:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,6 +45,9 @@ var Game = {
 		this.canvas = document.querySelector('canvas');
 		this.context = this.canvas.getContext('2d');
 
+		this.canvas.width = 900;
+		this.canvas.height = 500;
+
 		this.canvas.style.width = (this.canvas.width / 2) + 'px';
 		this.canvas.style.height = (this.canvas.height / 2) + 'px';
 
